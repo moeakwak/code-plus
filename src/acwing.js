cd5fe06270af77ddeb5e02706bc213e2b8c6f498
Moeakwak <moeakwak@gmail.com>
2022-03-22 15:44:40 +0800

    fix acwing info

--- src/acwing.js
+++ src/acwing.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 import $ from "jquery";
 import { html2md } from "./utils.js";
@@ -9,29 +9,46 @@ console.log("CodePlus acwing ok");
 
 let from = "acwing";
 let title_regex = /(\d+)\.(.*)/;
-let title_text = $("div[class='nice_font problem-content-title']").text().trim();
-let id = title_text.match(title_regex)[1].trim();
-let title = title_text.match(title_regex)[2].trim();
+let full_title = $("div[class='nice_font problem-content-title']")
+  .text()
+  .trim();
+let id = full_title.match(title_regex)[1].trim();
+let title = full_title.match(title_regex)[2].trim();
 let description = html2md($("[data-tab='preview-tab-content']").html(), from);
 let code = "";
-$("div[class='ace_layer ace_text-layer']").children().each(function() {code += $(this).text() + '\n'});
+$("div[class='ace_layer ace_text-layer']")
+  .children()
+  .each(function () {
+    code += $(this).text() + "\n";
+  });
+let code_language = $("select[name='language']").children(":selected").text().toLowerCase();
 let tags = [];
-$("div.problem-algorithm-tag-field").children("a").each(function() {tags.push($(this).text().trim())});
-let difficulty = $("#acwing_page > div > div > div > div > div.row > div.col-sm-3.hidden-xs > div > table > tbody > tr:nth-child(1) > td > span").text().trim();
+$("div.problem-algorithm-tag-field")
+  .children("a")
+  .each(function () {
+    tags.push($(this).text().trim());
+  });
+let difficulty = $(
+  "#acwing_page > div > div > div > div > div.row > div.col-sm-3.hidden-xs > div > table > tbody > tr:nth-child(1) > td > span"
+)
+  .text()
+  .trim();
 
 let data = {
   from,
-  id ,
+  id,
   title,
+  full_title,
+  difficulty,
   description,
   code,
-  difficulty
+  code_language,
+  tags,
 };
 
 console.log("CodePlus grabbed", data);
 
 chrome.runtime.sendMessage({
   type: "info",
-  data
+  data,
 });
-
